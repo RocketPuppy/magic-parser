700ab80d97491c9043932d5a305d4b7ba46fefb5
dwilson <dwilson@cibercare.com>
2012-06-12 23:00:50 -0400

    Merged new structures into old structures

--- Structures.hs
+++ Structures.hs
@@ -13,68 +13,71 @@
 -----------------------------------------------------------------------------
 
 module Structures (
--- $Examples
+
 ) where
 
--- $Examples
--- * Examples of the AST
--- Here are some examples of how the AST will look:
--- ** Abilities
--- [@Activated@]
--- > Activated (Pay [QuantifiedObject ([], Mana B)], [Destroy (QuantifiedObject ([Reference Target], Permanent)))
+data Card = Card
+    { abilities :: [Ability]
+    }
 
--- | A Quantified Object is an object with a list of quantifiers
--- | attached to it.
-data QuantifiedObject   = QuantifedObject ([Quantifier], Object)
-    deriving (Show)
+data Ability    = Spell     { effects :: [Effect]}
+                | Triggered { trigger :: Effect
+                            , condition :: Condition
+                            , mainEffects :: [Effect]
+                            , constraintEffects :: [Effect]
+                            }
+                | Activated { costs :: [Effect]
+                            , mainEffects :: [Effect]
+                            , constraintEffects :: [Effect]
+                            }
+-- | Effects are like the game's version of functions. They take parameters
+-- | and do something with them.
+-- | There are many effects in the game
+data Effect
+    = Pay                   PaymentType
+    | ZoneChange            Target Zone Zone
+    | Destroy               Target
+    | Sacrifice             Target
+    | Dies                  Target
+    | Counter               Target
+    | ETB                   Target
+    | LTB                   Target
 
--- | These are the quantifiers that can be attached to an object
-data Quantifier     = Reference         Reference
-                    | Count             Count
-                    | Characteristic    Characteristic
-                    | Status            Status
-    deriving (Show)
+-- | This is the representation of conditional statements
+data Condition
+    = If                    Condition
+    | Or                    Condition Condition
+    | And                   Condition Condition
+    | CEffect               Effect
+    | NoCondition
 
--- | These are the objects that can have quantifiers
-data Object         = Player            Player
-                    | Ability           Ability
-                    | Effect            Effect
-                    | Card
-                    | Permanent
-                    | Mana              Mana
-                    | Life
-    deriving (Show)
+data PaymentType = ManaPay [Mana] | LifePay Count
+
+data QuantifiedObject = Foo
+
+data Target =
+    Target  { targetCharacteristics :: [Characteristic]
+            , targetStatuses :: [Status]
+            , targetReferences :: [Reference]
+            , targetEffects :: [Effect]
+            , targetAbilities :: [Ability]
+            }
 
 -- | These are the various characteristics
 data Characteristic = Name              String
---                    | Mana              Cost [Mana]
+                    | CMC               Count
                     | Color             [Color]
-                    | Typestring        [Type]
+                    | Typestring        ([SuperType], [CardType], [SubType])
                     | Abilities         [Ability]
                     | Power             Count
                     | Toughness         Count
                     | Loyalty           Count
                     | Hand_Mod          Count
                     | Life_Mod          Count
-    deriving (Show)
-
--- | These are the various references
-data Reference      = You
-                    | Your
-                    | This
-                    | Target
-                    | ThisCard
-                    | Other
-    deriving (Show)
-
--- | This represents the 'count' of an object, with Digit being the natural
--- | representation as a number
-data Count          = Digit             Integer
-                    | All
-                    | Only
-                    | Single
-                    | None
-    deriving (Show)
+instance PrettyPrint Characteristic where
+    pp (Typestring (supers, cards, []))   = take ((length str) -1 ) (str)
+        where str = concatMap (\x -> (pp x) ++ " ") supers ++ concatMap (\x -> (pp x) ++ " ") cards
+    pp (Typestring (supers, cards, subs)) = concatMap (\x -> (pp x) ++ " ") supers ++ concatMap (\x -> (pp x) ++ " ") cards ++ "-" ++ concatMap (\x-> " " ++ (pp x)) subs
 
 -- | These are the various statuses an object can have
 data Status         = Tapped
@@ -85,90 +88,12 @@ data Status         = Tapped
                     | FaceDown
                     | PhasedIn
                     | PhasedOut
-    deriving (Show)
 
--- | These are the representations of different players
-data Player         = Opponent
-                    | Teammates
-                    | Self
-    deriving (Show)
-
--- | A quantified time is a way of representing when something should happen.
--- | The Maybe monads take care of the case when any of those isn't specified.
--- | The list can have nothing in it, in which case there isn't a Timing.
---data QuantifiedTime =
---     QuantifiedTime { times :: [Timing]
---                    , step  :: Maybe Step
---                    , phase :: Maybe Phase
---                    , turn  :: Maybe Turn
---                    }
---    deriving (Show)
-
-data QuantifiedTime = QuantifedTime ([Timing], Maybe Step, Maybe Phase, Maybe Turn)
-    deriving (Show)
-
--- | These are all of the phases of a turn
-data Phase          = Beginning
-                    | Main
-                    | Combat
-                    | Ending
-    deriving (Show)
-
--- | These are all the turns
-data Turn           = Next_Turn
-                    | Opponent_Turn
-                    | Your_Turn
-                    | This_Turn
-    deriving (Show)
-
--- | These are all the steps in a turn
-data Step           = Untap
-                    | Upkeep
-                    | Draw
-                    | Beginning_Combat
-                    | Declare_Attack
-                    | Declare_Block
-                    | Combat_Damage
-                    | End_Combat
-                    | End
-                    | Cleanup
-    deriving (Show)
-
--- | These are the timing quantifiers
-data Timing         = Next
-                    | Precombat
-                    | Postcombat
-    deriving (Show)
-
--- | This is the representation of abilities
-data Ability        = Spell                 Spell
-                    | Activated             Activated
-                    | Triggered             Triggered
-                    | Static                Static
-                    | Linked                Linked
-    deriving (Show)
-
--- | A spell ability is a list of instructions separated by periods.
-type Spell          = Instructions
-
--- | An activated ability has a (list of) costs, an effect, and optional
--- | instructions.
-type Activated      = (Costs, Effect, Instructions)
-
--- | A triggered ability has a trigger condition, an optional condition, an
--- | effect, and optional instructions.
-type Triggered      = (Trigger, Condition, Effect, Instructions)
-
--- | A loyalty ability is an activated ability on a planeswalker where the
--- | cost is a number of loyalty counters
-type Loyalty        = Activated
-
--- | A static ability is just an effect.
-type Static         = Effect
-
--- | A linked ability is one which references something that happens in a
--- | previous ability
-type Linked         = (Ability, [Ability])
+-- | These are the various references
+data Reference      = You
+                    | Your
+                    | This
+                    | Other
 
 -- | These are all the colors
 data Color          = White
@@ -177,62 +102,14 @@ data Color          = White
                     | Red
                     | Green
                     | Colorless
-    deriving (Show)
 
--- | A shorthand for multiple effects
-type Effects        = [Effect]
-
--- | Effects are like the game's version of functions. They take parameters
--- | and do something with them.
--- | There are many effects in the game
-data Effect         = Pay                   [QuantifiedObject]
-                    | ZoneChange            QuantifiedObject Zone Zone
-                    | Destroy               QuantifiedObject
-                    | Sacrifice             QuantifiedObject
-                    | Dies                  QuantifiedObject
-    deriving (Show)
-
--- | This is the representation of conditional statements
-data Condition      = If                    Condition
-                    | Or                    Condition Condition
-                    | And                   Condition Condition
-                    | CEffect               Effect
-                    | NoCondition
-    deriving (Show)
--- | Shorthand for multiple costs
-type Costs          = [Cost]
-
--- | A Cost can be modeled as an effect that has to happen in order for another
--- | effect to happen.
-type Cost           = Effect
-
--- | Shorthand for multiple instructions
-type Instructions   = [Instruction]
-
--- | Not implemented yet
-data Instruction    = Foo
-    deriving (Show)
-
--- | All the possible types of mana, including a arbitrary numbers of generic mana.
-data Mana           = W  | U  | B  | R  | G  | X
-                    | Generic               Count
-                    | WU | WB | UB | UR | BR | BG | RG | RW | GW | GU
-                    | W2 | U2 | B2 | R2 | G2
-                    | WP | UP | BP | RP | GP
-                    | S
-    deriving (Show)
-
--- | Like a cost, a trigger is an effect that must happen for something else to
--- | happen. The difference lies in that costs are optional, and triggers always
--- | happen.
-type Trigger        = Effect
-
--- | This is a representation of a single value of a typestring.
--- | A full typestring is a list of types.
-data Type           = SuperType             String
-                    | CardType              String
-                    | SubType               String
-    deriving (Show)
+-- | This represents the 'count' of an object, with Digit being the natural
+-- | representation as a number
+data Count          = Digit Integer
+                    | All
+                    | Only
+                    | Single
+                    | None
 
 -- | The possible zones
 data Zone           = Library
@@ -244,4 +121,117 @@ data Zone           = Library
                     | Exile
                     | Ante
                     | Anywhere
+
+-- | All the possible types of mana, including arbitrary numbers of generic mana.
+data Mana           = W  | U  | B  | R  | G  | X
+                    | Generic Count
+                    | WU | WB | UB | UR | BR | BG | RG | RW | GW | GU
+                    | W2 | U2 | B2 | R2 | G2
+                    | WP | UP | BP | RP | GP
+                    | S
     deriving (Show)
+instance PrettyPrint Mana where
+    pp (Generic (Digit i)) = "{" ++ (show i) ++ "}"
+    pp x = "{" ++ show x ++ "}"
+
+data SuperType  = Basic | Legendary | Ongoing | Snow | World
+    deriving (Show)
+instance PrettyPrint SuperType where
+    pp x = show x
+
+data CardType   = Artifact
+                | Creature
+                | Enchantment
+                | Instant
+                | Land
+                | Planeswalker
+                | Sorcery
+                | Tribal
+    deriving (Show)
+instance PrettyPrint CardType where
+    pp ct = show ct
+
+data SubType    = ArtifactType      ArtifactType
+                | EnchantmentType   EnchantmentType
+                | InstantType       InstantType
+                | LandType          LandType
+                | PlaneswalkerType  PlaneswalkerType
+                | CreatureType      CreatureType
+    deriving (Show)
+instance PrettyPrint SubType where
+    pp (ArtifactType x)     = pp x
+    pp (EnchantmentType x)  = pp x
+    pp (InstantType x)      = pp x
+    pp (LandType x)         = pp x
+    pp (PlaneswalkerType x) = pp x
+    pp (CreatureType x)     = pp x
+
+data ArtifactType    = Contraption | Equipment | Fortification
+    deriving (Show)
+instance PrettyPrint ArtifactType where
+    pp x = show x
+
+data EnchantmentType =  Aura | Curse | Shrine
+    deriving (Show)
+instance PrettyPrint EnchantmentType where
+    pp x = show x
+
+data InstantType = Arcane | Trap
+    deriving (Show)
+instance PrettyPrint InstantType where
+    pp x = show x
+
+data LandType = Desert | Forest | Island | Lair | Locus | Mine | Mountain
+              | Plains | PowerPlant | Swamp | Tower | Urzas
+    deriving (Show)
+instance PrettyPrint LandType where
+    pp x = show x
+
+data PlaneswalkerType   = Ajani | Bolas | Chandra | Elspeth | Garruk | Gideon | Jace
+                        | Karn | Koth | Liliana | Nissa | Sarkhan | Sorin | Tezzeret
+                        | Venser
+    deriving (Show)
+instance PrettyPrint PlaneswalkerType where
+    pp x = show x
+
+data CreatureType   = Advisor | Ally | Angel | Anteater | Antelope | Ape
+                    | Archer | Archon | Artificer | Assassin | AssemblyWorker
+                    | Atog | Aurochs | Avatar | Badger | Barbarian | Basilisk | Bat
+                    | Bear | Beast | Beeble | Berserker | Bird | Blinkmoth | Boar
+                    | Bringer | Brushwagg | Camarid | Camel | Caribou | Carrier
+                    | Cat | Centaur | Cephalid | Chimera | Citizen | Cleric
+                    | Cockatrice | Construct | Coward | Crab | Crocodile | Cyclops
+                    | Dauthi | Demon | Deserter | Devil | Djinn | Dragon | Drake
+                    | Dreadnought | Drone | Druid | Dryad | Dwarf | Efreet | Elder
+                    | Eldrazi | Elemental | Elephant | Elf | Elk | Eye | Faerie
+                    | Ferret | Fish | Flagbearer | Fox | Frog | Fungus | Gargoyle
+                    | Germ | Giant | Gnome | Goat | Goblin | Golem | Gorgon
+                    | Graveborn | Gremlin | Griffin | Hag | Harpy | Hellion | Hippo
+                    | Hippogriff | Homarid | Homunculus | Horror | Horse | Hound
+                    | Human | Hydra | Hyena | Illusion | Imp | Incarnation | Insect
+                    | Jellyfish | Juggernaut | Kavu | Kirin | Kithkin | Knight
+                    | Kobold | Kor | Kraken | Lammasu | Leech | Leviathan | Lhurgoyf
+                    | Licid | Lizard | Manticore | Masticore | Mercenary | Merfolk
+                    | Metathran | Minion | Minotaur | Monger | Mongoose | Monk
+                    | Moonfolk | Mutant | Myr | Mystic | Nautilus | Nephilim
+                    | Nightmare | Nightstalker | Ninja | Noggle | Nomad | Octopus
+                    | Ogre | Ooze | Orb | Orc | Orgg | Ouphe | Ox | Oyster | Pegasus
+                    | Pentavite | Pest | Phelddagrif | Phoenix | Pincher | Pirate
+                    | Plant | Praetor | Prism | Rabbit | Rat | Rebel | Reflection
+                    | Rhino | Rigger | Rogue | Salamander | Samurai | Sand | Saproling
+                    | Satyr | Scarecrow | Scorpion | Scout | Serf | Serpent | Shade
+                    | Shaman | Shapeshifter | Sheep | Siren | Skeleton | Slith
+                    | Sliver | Slug | Snake | Soldier | Soltari | Spawn | Specter
+                    | Spellshaper | Sphinx | Spider | Spike | Spirit | Splinter
+                    | Sponge | Squid | Squirrel | Starfish | Surrakar | Survivor
+                    | Tetravite | Thalakos | Thopter | Thrull | Treefolk
+                    | Triskelavite | Troll | Turtle | Unicorn | Vampire | Vedalken
+                    | Viashino | Volver | Wall | Warrior | Weird | Werewolf | Whale
+                    | Wizard | Wolf | Wolverine | Wombat | Worm | Wraith | Wurm
+                    | Yeti | Zombie | Zubera
+    deriving (Show)
+instance PrettyPrint CreatureType where
+    pp x = show x
+
+class PrettyPrint a where
+    pp :: a -> String
